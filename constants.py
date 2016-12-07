ae7642dd68fd4928fdf1dfc7179a9406a88a8a9d
sharkdp <davidpeter@web.de>
2016-12-07 20:11:56 +0100

    Adapted constants by dimensional reduction

--- constants.py
+++ constants.py
@@ -1,12 +1,11 @@
-POS_START = -10
-VEL_START = 0.1
+POS_START = -1
 MAX_ACC = 1
 MIN_ACC = -2
-MAX_ALLOWED_VEL = 5
-MAX_TIME = 20
+MAX_ALLOWED_VEL = 1
+VEL_START = MAX_ALLOWED_VEL
 DT = 0.001
 NUM_OBSERVATION_VARIABLES = 1
 
 # NN parameters
-STDDEV = 0.01 # Standard deviation of initial weight distribution
+STDDEV = 0.01  # Standard deviation of initial weight distribution
 LEARNING_PARAMETER = 0.01

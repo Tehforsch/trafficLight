5e9e113071f60af7205c411111b5f131ff1d256e
sharkdp <davidpeter@web.de>
2016-12-08 00:01:40 +0100

    Add visualization support for multiple strategies

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from trafficSim import TrafficSim
 from trafficLight import FixedTrafficLight
-from controller import ConstantSpeedController, LinearController, LateBrakeController
+from controller import LinearController, LateBrakeController, CheatController
 from visualization import showTrajectory
 
-# driver = ConstantSpeedController()
-# driver.train()
-# driver = LinearController()
-driver = LateBrakeController()
-s = TrafficSim(driver, FixedTrafficLight(5), logging=True)
-s.run()
-showTrajectory(s.log)
+driver1 = LinearController()
+s1 = TrafficSim(driver1, FixedTrafficLight(5), logging=True)
+s1.run()
+
+driver2 = LateBrakeController()
+s2 = TrafficSim(driver2, FixedTrafficLight(5), logging=True)
+s2.run()
+
+driver3 = CheatController()
+s3 = TrafficSim(driver3, FixedTrafficLight(5), logging=True)
+s3.run()
+
+showTrajectory([s1.log, s2.log, s3.log])
--- visualization.py
+++ visualization.py
@@ -3,41 +3,57 @@ import matplotlib.pyplot as plt
 from constants import POS_START, MIN_ACC, MAX_ACC, MAX_ALLOWED_VEL
 
 
-def showTrajectory(states):
-    times = list(s.time for s in states)
-    positions = list(s.pos for s in states)
-    velocities = list(s.vel for s in states)
-    accelerations = list(s.acc for s in states)
+def showTrajectory(logs):
+    times = [[s.time for s in log] for log in logs]
+    positions = [[s.pos for s in log] for log in logs]
+    velocities = [[s.vel for s in log] for log in logs]
+    accelerations = [[s.acc for s in log] for log in logs]
+    performances = [[s.pos + 0.5 * s.vel**2 / MAX_ACC for s in log] for log in logs]
 
-    maxTime = max(times)
+    maxTime = max(max(t) for t in times)
     deltaAcc = MAX_ACC - MIN_ACC
+    maxPerformance = 0.5 * MAX_ALLOWED_VEL**2 / MAX_ACC
 
-    plt.subplot(3, 1, 1)
+    plt.subplot(4, 1, 1)
 
     plt.title("position")
-    plt.plot(positions, times, linewidth=2)
+    for p, t in zip(positions, times):
+        plt.plot(p, t, linewidth=2)
     plt.ylabel("time")
     plt.xlim(POS_START * 1.1, 0.1 * abs(POS_START))
     plt.ylim(0, maxTime)
     plt.axvline(x=0, ymin=0, ymax=1, color='k')
     plt.axvline(x=POS_START, ymin=0, ymax=1, color='k')
 
-    plt.subplot(3, 1, 2)
+    plt.subplot(4, 1, 2)
 
     plt.title("velocity")
-    plt.plot(velocities, times, linewidth=2)
+    for v, t in zip(velocities, times):
+        plt.plot(v, t, linewidth=2)
     plt.ylabel("time")
     plt.xlim(-MAX_ALLOWED_VEL * 0.1, MAX_ALLOWED_VEL * 1.1)
     plt.ylim(0, maxTime)
     plt.axvline(x=0, ymin=0, ymax=1, color='k')
     plt.axvline(x=MAX_ALLOWED_VEL, ymin=0, ymax=1, color='k')
 
-    plt.subplot(3, 1, 3)
+    plt.subplot(4, 1, 3)
 
     plt.title("acceleration")
-    plt.plot(accelerations, times, linewidth=2)
+    for a, t in zip(accelerations, times):
+        plt.plot(a, t, linewidth=2)
     plt.ylabel("time")
     plt.xlim(MIN_ACC - 0.1 * deltaAcc, MAX_ACC + 0.1 * deltaAcc)
+    plt.ylim(0, maxTime)
+    plt.axvline(x=MIN_ACC, ymin=0, ymax=1, color='k')
+    plt.axvline(x=MAX_ACC, ymin=0, ymax=1, color='k')
+
+    plt.subplot(4, 1, 4)
+
+    plt.title("perfomance")
+    for p, t in zip(performances, times):
+        plt.plot(p, t, linewidth=2)
+    plt.ylabel("time")
+    plt.xlim(POS_START, maxPerformance)
     plt.ylim(0, maxTime)
     plt.axvline(x=MIN_ACC, ymin=0, ymax=1, color='k')
     plt.axvline(x=MAX_ACC, ymin=0, ymax=1, color='k')

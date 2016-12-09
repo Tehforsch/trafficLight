dbbdf29f8b2a4fc42bb8c63f440f6dee56720350
Tehforsch <Tonipeter92@googlemail.com>
2016-12-09 17:59:50 +0100

    rewrite main.py with a list

--- main.py
+++ main.py
@@ -3,16 +3,11 @@ from trafficLight import FixedTrafficLight
 from controller import LinearController, LateBrakeController, CheatController
 from visualization import showTrajectory
 
-driver1 = LinearController()
-s1 = TrafficSim(driver1, FixedTrafficLight(5), logging=True)
-s1.run()
+drivers = [LinearController(), LateBrakeController(), CheatController()]
+trafficLight = FixedTrafficLight(5)
 
-driver2 = LateBrakeController()
-s2 = TrafficSim(driver2, FixedTrafficLight(5), logging=True)
-s2.run()
+sims = [TrafficSim(driver, trafficLight, logging=True) for driver in drivers]
+for sim in sims:
+    sim.run()
 
-driver3 = CheatController()
-s3 = TrafficSim(driver3, FixedTrafficLight(5), logging=True)
-s3.run()
-
-showTrajectory([s1.log, s2.log, s3.log])
+showTrajectory([sim.log for sim in sims])

a64771ccc91b593daa9585227a78c191c1adaab5
Tehforsch <Tonipeter92@googlemail.com>
2016-12-13 18:29:22 +0100

    use genetic optimization

--- main.py
+++ main.py
@@ -4,24 +4,28 @@ from controller import LinearController, PowerLawController, \
     LateBrakeController, CheatController
 from visualization import showTrajectory
 import evaluation
+import geneticOptimization
+from constants import MAX_TIME
 
-drivers = [LinearController(), 
-           LateBrakeController(),
-           PowerLawController(0.5), 
-           PowerLawController(3.0), 
-           CheatController()]
-trafficLight = UniformTrafficLight(5)
+trafficLight = UniformTrafficLight(MAX_TIME)
+geneticOptimization.optimize(trafficLight)
 
-sims = [TrafficSim(driver, trafficLight.maxTime, logging=True) for driver in drivers]
-for sim in sims:
-    sim.run()
+# drivers = [LinearController(), 
+#            LateBrakeController(),
+#            PowerLawController(0.5), 
+#            PowerLawController(3.0), 
+#            CheatController()]
+# sims = [TrafficSim(driver, trafficLight.maxTime, logging=True) for driver in drivers]
+# for sim in sims:
+#     sim.run()
 
-def printTotalScores(sims):
-    print("Total scores:")
-    for sim in sims:
-        name = str(sim.driver)
-        total = evaluation.totalPerformance(sim, trafficLight)
-        print("{}: {}".format(name, total))
+# def printTotalScores(sims):
+#     print("Total scores:")
+#     for sim in sims:
+#         name = str(sim.driver)
+#         total = evaluation.totalPerformance(sim, trafficLight)
+#         print("{}: {}".format(name, total))
 
-printTotalScores(sims)
-showTrajectory([sim.log for sim in sims])
+# printTotalScores(sims)
+# showTrajectory([sim.log for sim in sims])
+

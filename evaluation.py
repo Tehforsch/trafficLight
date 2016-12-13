7058bf163125ba851ade6c80acd9397b6aacddf2
Tehforsch <Tonipeter92@googlemail.com>
2016-12-13 18:24:32 +0100

    fix a bug (wrong dimension in the performance function)

--- evaluation.py
+++ evaluation.py
@@ -4,10 +4,14 @@ def totalPerformance(sim, trafficLight):
     return trafficLight.score(performance(sim.log))
 
 def performance(log):
+    return [localPerformance(state) for state in log]
+
+def localPerformance(state):
     epsilon = DT * MAX_VEL
-    if log[-1].pos > epsilon:
+    if state.pos > epsilon:
         return RUNNING_RED_LIGHT_PUNISHMENT
-    return [s.pos - 0.5 * (MAX_VEL - s.vel)**2 / MAX_ACC for s in log]
+    else:
+        return state.pos - 0.5 * (MAX_VEL - state.vel)**2 / MAX_ACC
 
 def minPerformance():
     return START_POS - 0.5 * MAX_VEL**2 / MAX_ACC

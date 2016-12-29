ef3429c831453c42210abdaf964f8bcd2d7a33d2
sharkdp <davidpeter@web.de>
2016-12-29 23:22:00 +0100

    Remove legacy NN code

--- constants.py
+++ constants.py
@@ -17,8 +17,3 @@ NUM_STEPS = int(MAX_TIME / DT)
 # Learning/performance parameters
 RUNNING_RED_LIGHT_EPSILON = 1e-4
 RUNNING_RED_LIGHT_PUNISHMENT = -100
-
-# NN parameters
-NUM_OBSERVATION_VARIABLES = 1
-STDDEV = 0.01  # Standard deviation of initial weight distribution
-LEARNING_PARAMETER = 0.01
